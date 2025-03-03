827c3ea0787e633ef9d691a414fba25d642bca3d
HackerX <133592141+AnasAli30@users.noreply.github.com>
2025-03-03 14:14:45 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,16 @@ const nextConfig = {
   experimental: {
     esmExternals: "loose",
   },
- webpack: (config) => {
-      config.externals.push('pino-pretty', 'lokijs', 'encoding');
-      return config;
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
